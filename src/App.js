e31a81ff2717f8ef4ec9d2fdb1fe42d8c922a5ab
Dan Nguyen <dnguye03@uw.edu>
2024-11-04 15:23:50 -0800

    Followed tutorials, modified README
    Followed the basic boardgame.io tutorial and the connect 4 tutorial up until "Rendering the board and accepting user input."

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+    adapted from tutorials by Patrick Johannessen 
+        https://www.lonesomecrowdedweb.com/blog/four-in-a-row-boardgameio/
+    and boardgame.io
+        https://boardgame.io/documentation/#/tutorial
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Client } from 'boardgame.io/react';
+import { ConnectFour } from './Game';
+import { ConnectFourBoard } from './Board';
+
+const App = Client({
+  game: ConnectFour,
+  //board: ConnectFourBoard,
+});
 
 export default App;
---
+++ src/winCondition.js
@@ -0,0 +1,61 @@
+/*
+    adapted from tutorials by Patrick Johannessen 
+        https://www.lonesomecrowdedweb.com/blog/four-in-a-row-boardgameio/
+    and boardgame.io
+        https://boardgame.io/documentation/#/tutorial
+*/
+
+// returns true if grid is in a winning configuration
+function isVictory(grid, player) {
+    const playerDisc = playerDiscLookup[player];
+
+    // victory algorithm by ferdelOlmo: https://stackoverflow.com/a/38211417/129967
+    // adapted by Patrick Johannessen: https://www.lonesomecrowdedweb.com/blog/four-in-a-row-boardgameio/
+    let row = 0;
+    let column = 0;
+
+    // horizontalCheck
+    for (column = 0; column < numOfColumns - 3; column++) {
+        for (row = 0; row < numOfRows; row++) {
+            if (grid[row][column] === playerDisc && grid[row][column + 1] === playerDisc && grid[row][column + 2] === playerDisc && grid[row][column + 3] === playerDisc) {
+                return true;
+            }
+        }
+    }
+
+    // verticalCheck
+    for (row = 0; row < numOfRows - 3; row++) {
+        for (column = 0; column < numOfColumns; column++) {
+            if (grid[row][column] === playerDisc && grid[row + 1][column] === playerDisc && grid[row + 2][column] === playerDisc && grid[row + 3][column] === playerDisc) {
+                return true;
+            }
+        }
+    }
+
+    // ascendingDiagonalCheck
+    for (row = 3; row < numOfRows; row++) {
+        for (column = 0; column < numOfColumns - 3; column++) {
+            if (grid[row][column] === playerDisc && grid[row - 1][column + 1] === playerDisc && grid[row - 2][column + 2] === playerDisc && grid[row - 3][column + 3] === playerDisc) {
+                return true;
+            }
+        }
+    }
+
+    // descendingDiagonalCheck
+    for (row = 3; row < numOfRows; row++) {
+        for (column = 3; column < numOfColumns; column++) {
+            if (grid[row][column] === playerDisc && grid[row - 1][column - 1] === playerDisc && grid[row - 2][column - 2] === playerDisc && grid[row - 3][column - 3] === playerDisc) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+// returns true if grid is completely occupied
+function isDraw(grid) {
+    return grid.filter(c => c === emptyCell).length === 0;
+}
+
+export { isVictory, isDraw };

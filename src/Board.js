c8bd8bae717e84c5f2c6847a84b6941743c1ea9c
Dan Nguyen <dnguye03@uw.edu>
2024-11-06 23:58:29 -0800

    Board.js rewrite
    Changed the logic of the click event listener and adding the table for the board; Added functional component Cell.
    Added tentative goals.

--- src/Board.js
+++ src/Board.js
@@ -1,101 +1,79 @@
-// /*
-//     adapted from tutorials by Patrick Johannessen 
-//         https://www.lonesomecrowdedweb.com/blog/four-in-a-row-boardgameio/
-//     and boardgame.io
-//         https://boardgame.io/documentation/#/tutorial
-// */
-
-// import React from 'react';
-
-// export function ConnectFourBoard({ ctx, G, moves }) {
-//     const onClick = (id) => moves.clickCell(id);
-
-//     let winner = '';
-//     if (ctx.gameover) {
-//         winner =
-//             ctx.gameover.winner !== undefined ? (
-//                 <div id="winner">Winner: {ctx.gameover.winner}</div>
-//             ) : (
-//                 <div id="winner">Draw!</div>
-//             );
-//     }
-
-//     const cellStyle = {
-//         border: '1px solid #555',
-//         width: '50px',
-//         height: '50px',
-//         lineHeight: '50px',
-//         textAlign: 'center',
-//     };
-
-//     let tbody = [];
-//     for (let i = 0; i < 3; i++) {
-//         let cells = [];
-//         for (let j = 0; j < 3; j++) {
-//             const id = 3 * i + j;
-//             cells.push(
-//                 <td key={id}>
-//                     {G.cells[id] ? (
-//                         <div style={cellStyle}>{G.cells[id]}</div>
-//                     ) : (
-//                         <button style={cellStyle} onClick={() => onClick(id)} />
-//                     )}
-//                 </td>
-//             );
-//         }
-//         tbody.push(<tr key={i}>{cells}</tr>);
-//     }
-
-//     return (
-//         <div>
-//             <table id="board">
-//                 <tbody>{tbody}</tbody>
-//             </table>
-//             {winner}
-//         </div>
-//     );
-// }
-
+/*
+adapted from tutorials by Patrick Johannessen 
+https://www.lonesomecrowdedweb.com/blog/four-in-a-row-boardgameio/
+and boardgame.io
+https://boardgame.io/documentation/#/tutorial
+*/
 
 import React from "react";
-import { emptyCell, p1disc, p2disc } from "./constants";
+import { emptyCell, p1disc, p2disc, numOfRows, numOfColumns, playerDiscLookup } from "./constants";
+import WhiteDisc from './circle-white.svg';
+import BlueDisc from './circle-blue.svg';
+import RedDisc from './circle-red.svg';
+
+// takes in a cell's attribute and checks its value
+const Cell = ({ cell }) => {
+    let cellImg;
+    let cellStr;
+    switch (cell) {
+        case p1disc:
+            cellStr = "p1 disc";
+            cellImg = RedDisc;
+            break;
+        case p2disc:
+            cellStr = "p2 disc";
+            cellImg = BlueDisc;
+            break;
+        default:
+            cellStr = "no disc";
+            cellImg = WhiteDisc;
+            break;
+    }
+    return (
+        <img alt={cellStr} src={cellImg} className="disc" />
+    );
+}
 
 export function ConnectFourBoard({ ctx, G, moves }) {
-  const onClick = (columnIdx) => {
-    moves.clickColumn(columnIdx); // Directly call clickColumn with column index
-  };
+    // call the click event handler with the keys in the 2D array
+    const clickColumn = (clickedCol) => moves.clickColumn(clickedCol);
 
-  let winner = "";
-  if (ctx.gameover) {
-    winner = ctx.gameover.winner !== undefined ? <div id="winner">Winner: {ctx.gameover.winner}</div> : <div id="winner">Draw!</div>;
-  }
-
-  const cellStyle = {
-    border: "1px solid #555",
-    width: "50px",
-    height: "50px",
-    lineHeight: "50px",
-    textAlign: "center",
-    cursor: "pointer",
-  };
-
-  let tbody = [];
-  for (let i = 0; i < 6; i++) {
-    let cells = [];
-    for (let j = 0; j < 7; j++) {
-      cells.push(
-        <td key={`${i}-${j}`}>{G.grid[i][j] !== emptyCell ? <div style={cellStyle}>{G.grid[i][j] === p1disc ? "🔴" : "🔵"}</div> : <button style={cellStyle} onClick={() => onClick(j)} />}</td>
-      );
+    // create a 2D array where [0][0] is the top leftmost corner and [numOfRows][numOfColumns] is the bottom rightmost corner
+    let boardBody = [];
+    for (let row = 0; row < numOfRows; row++) {
+        let cells = [];
+        for (let column = 0; column < numOfColumns; column++) {
+            // fill table with table data that passes its column number when clicked on 
+            //   and uses the functional component Cell to return the appropriate image
+            cells.push(
+                <td key={column} className="discdata" onClick={() => clickColumn(column)}>
+                    <Cell cell={G.grid[row][column]}/>
+                </td>
+            );
+        }
+        // push the completed row into the array
+        boardBody.push(<tr key={row}>{cells}</tr>);
     }
-    tbody.push(<tr key={i}>{cells}</tr>);
-  }
 
-  return (
-    <div>
-      <table id="board">
-        <tbody>{tbody}</tbody>
-      </table>
-      {winner}
-    </div>
-  );
+    return (
+        <div>
+            <table id="board">
+                <tbody>{boardBody}</tbody>
+            </table>
+
+        </div>
+    );
+    
+    /* TO DO (tentative):
+            display players at the bottom (refer to mockup)
+            display whose turn it is (use "Your turn" for player's side, username for other player)
+            have available slots glow (refer to mockup)
+    */
+
+    /*
+    let winner = "";
+    if (ctx.gameover) {
+        winner = ctx.gameover.winner !== undefined ? <div id="winner">Winner: {ctx.gameover.winner}</div> : <div id="winner">Draw!</div>;
+    }
+    */
 }

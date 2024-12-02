5e7aa30f43785b645bec1ac0d9b06da96f432460
ianm3039UW <ianm3039@uw.edu>
2024-12-02 13:54:34 -0800

    Added CSS Style

--- src/Pages/MultiplayerPage.jsx
+++ src/Pages/MultiplayerPage.jsx
@@ -10,46 +10,46 @@ const ConnectFourClient = Client({
     game: ConnectFour(),
     board: ConnectFourBoard,
     multiplayer: SocketIO({ server: 'http://localhost:8000' }),
-    debug: false
+    debug: true
   });
 
 function MultiplayerPage() {
     const [playerName, setPlayerName] = useState(null);
   const [playerID, setPlayerID] = useState(null);
   const [matchID, setMatchID] = useState(null);
-
-  const onMatchJoined = (name, matchID, isCreator) => {
-    setPlayerName(name);
-    setMatchID(matchID);
-    // Assign player ID based on whether the player is creating or joining
-    setPlayerID(isCreator ? '0' : '1');
-  };
-
-  return (
-    <div style={fullDisplay}>
-      <div className="header" style={headerStyle}>
-        <p>Connect Four Online</p>
-      </div>
-
-      <div style={mainStyle}>
-        {!playerID ? (
-          <LobbySetup onMatchJoined={onMatchJoined} />
-        ) : (
-          <div style={boardFlexStyle}>
-            <ConnectFourClient
-              playerID={playerID}
-              matchID={matchID}
-              credentials={playerName}
-            />
+  
+    const onMatchJoined = (name, matchID, isCreator) => {
+      setPlayerName(name);
+      setMatchID(matchID);
+      // Assign player ID based on whether the player is creating or joining
+      setPlayerID(isCreator ? '0' : '1');
+    };
+  
+    return (
+      <div style={fullDisplay}>
+        <div className="header" style={headerStyle}>
+          <p>Connect Four Online</p>
+        </div>
+  
+        <div style={mainStyle}>
+          {!playerID ? (
+            <LobbySetup onMatchJoined={onMatchJoined} />
+          ) : (
+            <div style={boardFlexStyle}>
+              <ConnectFourClient
+                playerID={playerID}
+                matchID={matchID}
+                credentials={playerName}
+              />
+            </div>
+          )}
+  
+          <div style={chatFlexStyle}>
+            {/* Chat functionality or additional components can be added here */}
           </div>
-        )}
-
-        <div style={chatFlexStyle}>
-          {/* Chat functionality or additional components can be added here */}
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 const fullDisplay = {
